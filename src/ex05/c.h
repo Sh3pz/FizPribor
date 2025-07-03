c875686904426a638490c2b606f72b50c729e985
Shypz <kirillvatetin@yandex.ru>
2025-07-03 13:22:26 +0300

    develop ex05 functions in c.c and c.h files

---
+++ src/ex05/c.h
@@ -0,0 +1,50 @@
+/******************************//*!
+ * \file
+ * \brief	Описание
+ * \author
+ * \date	Создан:
+ * \date	Изменён:
+ */
+
+#ifndef C_H
+#define C_H
+
+
+/*
+ *	Macros definition
+ */
+
+
+/*
+ *	Type declaration
+ */
+
+
+/*
+ *	Data declaration
+ */
+
+
+/*
+ *	Function declaration
+ */
+#include <stdio.h>
+void printArrUInt(unsigned int arr[], size_t size);
+void printArrShInt(short arr[], size_t size);
+void printArrInt(int arr[], size_t size);
+void printArrLong(long arr[], size_t size);
+void printArrLongLong(long long arr[], size_t size);
+void printArrChar(char arr[], size_t size);
+void printArrFloat(float arr[], size_t size);
+void printArrDouble(double arr[], size_t size);
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* _NAME_H */
--- src/ex05/main.c
+++ src/ex05/main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include "c.h"
 
 struct myStruct {
     unsigned int uInt;
@@ -35,15 +36,6 @@ union myEqualUnion {
     double dNum[sizeof(long double)/sizeof(double)];
     long double ldNum;
 };
-
-void printArrUInt(unsigned int arr[], size_t size);
-void printArrShInt(short arr[], size_t size);
-void printArrInt(int arr[], size_t size);
-void printArrLong(long arr[], size_t size);
-void printArrLongLong(long long arr[], size_t size);
-void printArrChar(char arr[], size_t size);
-void printArrFloat(float arr[], size_t size);
-void printArrDouble(double arr[], size_t size);
 
 void main(void) {
     struct myStruct strct = {62345, -10, 25, -335305, 44034030403, 'A', 3.14f, 6.2435, 934.1434345};
@@ -85,44 +77,4 @@ void main(void) {
     printArrFloat(eUnn.fNum, sizeof(eUnn.fNum)/sizeof(float));
     printArrDouble(eUnn.dNum, sizeof(eUnn.dNum)/sizeof(double));
     printf("Адрес поля ldNum = %p, его значение = %Lf, его размер = %zu\n\n", &unn.ldNum, unn.ldNum, sizeof(unn.ldNum));
-}
-
-void printArrUInt(unsigned int arr[], size_t size) {
-    printf("Адрес поля uInt = %p\n", &arr);
-    for (int i = 0; i < size; i++) printf("Значение %d-го элемента массива uInt = %u\n", i, arr[i]);
-}
-
-void printArrShInt(short arr[], size_t size) {
-    printf("Адрес поля shNum = %p\n", arr);
-    for (size_t i = 0; i < size; i++) printf("Значение %zu-го элемента массива shNum = %hd\n", i, arr[i]);
-}
-
-void printArrInt(int arr[], size_t size) {
-    printf("Адрес поля num = %p\n", arr);
-    for (size_t i = 0; i < size; i++) printf("Значение %zu-го элемента массива num = %d\n", i, arr[i]);
-}
-
-void printArrLong(long arr[], size_t size) {
-    printf("Адрес поля lNum = %p\n", arr);
-    for (size_t i = 0; i < size; i++) printf("Значение %zu-го элемента массива lNum = %ld\n", i, arr[i]);
-}
-
-void printArrLongLong(long long arr[], size_t size) {
-    printf("Адрес поля llNum = %p\n", arr);
-    for (size_t i = 0; i < size; i++) printf("Значение %zu-го элемента массива llNum = %lld\n", i, arr[i]);
-}
-
-void printArrChar(char arr[], size_t size) {
-    printf("Адрес поля symb = %p\n", arr);
-    for (size_t i = 0; i < size; i++) printf("Значение %zu-го элемента массива symb = %c\n", i, arr[i]);
-}
-
-void printArrFloat(float arr[], size_t size) {
-    printf("Адрес поля fNum = %p\n", arr);
-    for (size_t i = 0; i < size; i++) printf("Значение %zu-го элемента массива fNum = %f\n", i, arr[i]);
-}
-
-void printArrDouble(double arr[], size_t size) {
-    printf("Адрес поля dNum = %p\n", arr);
-    for (size_t i = 0; i < size; i++) printf("Значение %zu-го элемента массива dNum = %f\n", i, arr[i]);
 }
