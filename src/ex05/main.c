b89575c2a162e0896b93540b68bf2b4d86953f1b
Shypz <kirillvatetin@yandex.ru>
2025-07-03 13:15:52 +0300

    develop ex05 functions after main

--- src/ex05/main.c
+++ src/ex05/main.c
@@ -36,45 +36,14 @@ union myEqualUnion {
     long double ldNum;
 };
 
-void printArrUInt(unsigned int arr[], size_t size) {
-    printf("Адрес поля uInt = %p\n", &arr);
-    for (int i = 0; i < size; i++) printf("Значение %d-го элемента массива uInt = %u\n", i, arr[i]);
-}
-
-void printArrShInt(short arr[], size_t size) {
-    printf("Адрес поля shNum = %p\n", arr);
-    for (size_t i = 0; i < size; i++) printf("Значение %zu-го элемента массива shNum = %hd\n", i, arr[i]);
-}
-
-void printArrInt(int arr[], size_t size) {
-    printf("Адрес поля num = %p\n", arr);
-    for (size_t i = 0; i < size; i++) printf("Значение %zu-го элемента массива num = %d\n", i, arr[i]);
-}
-
-void printArrLong(long arr[], size_t size) {
-    printf("Адрес поля lNum = %p\n", arr);
-    for (size_t i = 0; i < size; i++) printf("Значение %zu-го элемента массива lNum = %ld\n", i, arr[i]);
-}
-
-void printArrLongLong(long long arr[], size_t size) {
-    printf("Адрес поля llNum = %p\n", arr);
-    for (size_t i = 0; i < size; i++) printf("Значение %zu-го элемента массива llNum = %lld\n", i, arr[i]);
-}
-
-void printArrChar(char arr[], size_t size) {
-    printf("Адрес поля symb = %p\n", arr);
-    for (size_t i = 0; i < size; i++) printf("Значение %zu-го элемента массива symb = %c\n", i, arr[i]);
-}
-
-void printArrFloat(float arr[], size_t size) {
-    printf("Адрес поля fNum = %p\n", arr);
-    for (size_t i = 0; i < size; i++) printf("Значение %zu-го элемента массива fNum = %f\n", i, arr[i]);
-}
-
-void printArrDouble(double arr[], size_t size) {
-    printf("Адрес поля dNum = %p\n", arr);
-    for (size_t i = 0; i < size; i++) printf("Значение %zu-го элемента массива dNum = %f\n", i, arr[i]);
-}
+void printArrUInt(unsigned int arr[], size_t size);
+void printArrShInt(short arr[], size_t size);
+void printArrInt(int arr[], size_t size);
+void printArrLong(long arr[], size_t size);
+void printArrLongLong(long long arr[], size_t size);
+void printArrChar(char arr[], size_t size);
+void printArrFloat(float arr[], size_t size);
+void printArrDouble(double arr[], size_t size);
 
 void main(void) {
     struct myStruct strct = {62345, -10, 25, -335305, 44034030403, 'A', 3.14f, 6.2435, 934.1434345};
@@ -116,4 +85,44 @@ void main(void) {
     printArrFloat(eUnn.fNum, sizeof(eUnn.fNum)/sizeof(float));
     printArrDouble(eUnn.dNum, sizeof(eUnn.dNum)/sizeof(double));
     printf("Адрес поля ldNum = %p, его значение = %Lf, его размер = %zu\n\n", &unn.ldNum, unn.ldNum, sizeof(unn.ldNum));
+}
+
+void printArrUInt(unsigned int arr[], size_t size) {
+    printf("Адрес поля uInt = %p\n", &arr);
+    for (int i = 0; i < size; i++) printf("Значение %d-го элемента массива uInt = %u\n", i, arr[i]);
+}
+
+void printArrShInt(short arr[], size_t size) {
+    printf("Адрес поля shNum = %p\n", arr);
+    for (size_t i = 0; i < size; i++) printf("Значение %zu-го элемента массива shNum = %hd\n", i, arr[i]);
+}
+
+void printArrInt(int arr[], size_t size) {
+    printf("Адрес поля num = %p\n", arr);
+    for (size_t i = 0; i < size; i++) printf("Значение %zu-го элемента массива num = %d\n", i, arr[i]);
+}
+
+void printArrLong(long arr[], size_t size) {
+    printf("Адрес поля lNum = %p\n", arr);
+    for (size_t i = 0; i < size; i++) printf("Значение %zu-го элемента массива lNum = %ld\n", i, arr[i]);
+}
+
+void printArrLongLong(long long arr[], size_t size) {
+    printf("Адрес поля llNum = %p\n", arr);
+    for (size_t i = 0; i < size; i++) printf("Значение %zu-го элемента массива llNum = %lld\n", i, arr[i]);
+}
+
+void printArrChar(char arr[], size_t size) {
+    printf("Адрес поля symb = %p\n", arr);
+    for (size_t i = 0; i < size; i++) printf("Значение %zu-го элемента массива symb = %c\n", i, arr[i]);
+}
+
+void printArrFloat(float arr[], size_t size) {
+    printf("Адрес поля fNum = %p\n", arr);
+    for (size_t i = 0; i < size; i++) printf("Значение %zu-го элемента массива fNum = %f\n", i, arr[i]);
+}
+
+void printArrDouble(double arr[], size_t size) {
+    printf("Адрес поля dNum = %p\n", arr);
+    for (size_t i = 0; i < size; i++) printf("Значение %zu-го элемента массива dNum = %f\n", i, arr[i]);
 }
